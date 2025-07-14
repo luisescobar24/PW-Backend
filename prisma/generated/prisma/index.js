bba5c6c16e6d79f453f37758b6fbb23e01793299
luisescobar24 <151747259+luisescobar24@users.noreply.github.com>
2025-07-14 07:36:41 -0500

    v4

--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -222,7 +222,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../..",
   "clientVersion": "6.10.1",
